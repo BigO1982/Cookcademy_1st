01ec4d5d881ea8aab3bec9fe12e6e81ac7768150
BigO <noreply>
2023-09-28 14:01:08 -0700

    Initial commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Oliver Hivale on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
