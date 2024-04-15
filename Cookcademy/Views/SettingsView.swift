b3c6c041df99d1f9e65c26b2caea96fef14e107c
BigO <noreply>
2024-04-15 14:44:31 -0700

    working on cookcademy 10

---
+++ Cookcademy/Views/SettingsView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsView.swift
+//  Cookcademy
+//
+//  Created by Oliver Hivale on 4/15/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @State private var hideOptionalSteps: Bool = false
+    @State private var listBackgroundColor = AppColor.background
+    @State private var listTextColor = AppColor.foreground
+    
+    var body: some View {
+        NavigationView {
+          Form {
+            ColorPicker("List BackgroundColor", selection: $listBackgroundColor)
+              .padding()
+              .listRowBackground(listBackgroundColor)
+            ColorPicker("Text Color", selection: $listTextColor)
+              .padding()
+              .listRowBackground(listBackgroundColor)
+            Toggle("Hide Optional Steps", isOn: $hideOptionalSteps)
+              .padding()
+              .listRowBackground(listBackgroundColor)
+          }
+          .foregroundColor(listTextColor)
+          .navigationTitle("Settings")
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
