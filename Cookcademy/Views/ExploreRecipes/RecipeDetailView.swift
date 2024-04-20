0cb3d713c7c8f4d26e6e1e54a2fbb7d689665a39
BigO <noreply>
2024-04-19 19:37:53 -0700

    finished Cookcademy 10

--- Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipeDetailView.swift
@@ -8,78 +8,83 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
-  @Binding var recipe: Recipe
+    @AppStorage("hideOptionalSteps") private var hideOptionalSteps: Bool = false
+    @Binding var recipe: Recipe
     
-  @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
-  @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
+    @AppStorage("listBackgroundColor") private var listBackgroundColor = AppColor.background
+    @AppStorage("listTextColor") private var listTextColor = AppColor.foreground
     
-  @State private var isPresenting = false
+    @State private var isPresenting = false
     
-  var body: some View {
-    VStack {
-      HStack {
-        Text("Author: \(recipe.mainInformation.author)")
-          .font(.subheadline)
-          .padding()
-        Spacer()
-      }
-      HStack {
-        Text(recipe.mainInformation.description)
-          .font(.subheadline)
-          .padding()
-        Spacer()
-      }
-      List {
-        Section(header: Text("Ingredients")) {
-          ForEach(recipe.ingredients.indices, id: \.self) { index in
-            let ingredient = recipe.ingredients[index]
-            Text(ingredient.description)
-              .foregroundColor(listTextColor)
-          }
-        }.listRowBackground(listBackgroundColor)
-        Section(header: Text("Directions")) {
-            ForEach(recipe.directions.indices, id: \.self) { index in
-              let direction = recipe.directions[index]
-              HStack {
-                Text("\(index + 1). ").bold()
-                Text("\(direction.isOptional ? "(Optional) " : "")"
-                      + "\(direction.description)")
-              }.foregroundColor(listTextColor)
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Author: \(recipe.mainInformation.author)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
             }
-        }.listRowBackground(listBackgroundColor)
-      }
-    }
-    .navigationTitle(recipe.mainInformation.name)
-    .toolbar {
-        ToolbarItem {
-          HStack {
-            Button("Edit") {
-              isPresenting = true
+            HStack {
+                Text(recipe.mainInformation.description)
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
             }
-            Button(action: {
-              recipe.isFavorite.toggle()
-            }) {
-              Image(systemName: recipe.isFavorite ? "heart.fill" : "heart")
-            }
-          }
-        }
-    }
-    .sheet(isPresented: $isPresenting) {
-        NavigationView {
-          ModifyRecipeView(recipe: $recipe)
-            .toolbar {
-              ToolbarItem(placement: .confirmationAction) {
-                Button("Save") {
-                  isPresenting = false
+            List {
+                Section(header: Text("Ingredients")) {
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                        let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                            .foregroundColor(listTextColor)
+                    }
+                }.listRowBackground(listBackgroundColor)
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let direction = recipe.directions[index]
+                        if direction.isOptional && hideOptionalSteps {
+                            EmptyView()
+                        } else {
+                            HStack {
+                                let index = recipe.index(of: direction, excludingOptionalDirections: hideOptionalSteps) ?? 0
+                                Text("\(index + 1). ").bold()
+                                Text("\(direction.isOptional ? "(Optional) " : "")\(direction.description)")
+                                
+                            }.foregroundColor(listTextColor)
+                        }
+                    }.listRowBackground(listBackgroundColor)
                 }
-              }
             }
-            .navigationTitle("Edit Recipe")
+            .navigationTitle(recipe.mainInformation.name)
+            .toolbar {
+                ToolbarItem {
+                    HStack {
+                        Button("Edit") {
+                            isPresenting = true
+                        }
+                        Button(action: {
+                            recipe.isFavorite.toggle()
+                        }) {
+                            Image(systemName: recipe.isFavorite ? "heart.fill" : "heart")
+                        }
+                    }
+                }
+            }
+            .sheet(isPresented: $isPresenting) {
+                NavigationView {
+                    ModifyRecipeView(recipe: $recipe)
+                        .toolbar {
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Save") {
+                                    isPresenting = false
+                                }
+                            }
+                        }
+                        .navigationTitle("Edit Recipe")
+                }
+            }
         }
     }
-  }
 }
-
 struct RecipeDetailView_Previews: PreviewProvider {
   @State static var recipe = Recipe.testRecipes[0]
   static var previews: some View {
